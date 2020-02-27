5e5f2d10887cab4cfcfe97bf9d6e5105950a7aaf
Sergio Alfonso <salfonso@conexia.com>
2020-02-27 11:56:54 -0500

    ejb3-server-api

---
+++ ejb3-server-api/src/main/java/com/letsprog/learning/ejb3/server/api/ILocalQuizQuestionGenerator.java
@@ -0,0 +1,7 @@
+package com.letsprog.learning.ejb3.server.api;
+
+import com.letsprog.learning.ejb3.server.api.entities.LevelQuestion;
+
+public interface ILocalQuizQuestionGenerator {
+	LevelQuestion generateQuestion(int level);
+}
---
+++ ejb3-server-api/src/main/java/com/letsprog/learning/ejb3/server/api/IPlayedQuizzesCounter.java
@@ -0,0 +1,6 @@
+package com.letsprog.learning.ejb3.server.api;
+
+public interface IPlayedQuizzesCounter {
+	void increment();
+	long getNumber();
+}
